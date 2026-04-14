8d3f23f091baf1563e03d1b341347b29e454f563
Ritabrata Basak <ritabratabasak2005@gmail.com>
2026-04-14 13:25:20 +0530

    edit env.py

--- orm/alembic/env.py
+++ orm/alembic/env.py
@@ -1,4 +1,5 @@
 from logging.config import fileConfig
+import os
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
@@ -10,7 +11,7 @@ from app.models import Base, Post, User, Votes
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url", "postgresql://postgres:ilovemando@localhost/fastapi")
+config.set_main_option("sqlalchemy.url", os.getenv("DATABASE_URL"))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
